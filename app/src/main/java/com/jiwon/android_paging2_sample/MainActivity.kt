e69797355e0646023ed5c7d30a30e06f7764f4c5
JiwonHae <hjiwon1994@gmail.com>
2022-04-10 23:21:14 +0900

    PROJ : first commit

---
+++ app/src/main/java/com/jiwon/android_paging2_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jiwon.android_paging2_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
